430fb933f137654e8eaf0cfe5dfe522f7482608d
roszelj <justin@elementwebtech.com>
2011-11-12 16:26:20 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalonfousheeCom::Application.config.secret_token = '7022dda7ba46a467f7c12ced6eaba0c2d687b253c02992277054cd1c2f93ea500d855d1745b0f7ab170a7d2bcf3350d20c5a8c05ff01fa8d9a872e4ea6bc9533'
