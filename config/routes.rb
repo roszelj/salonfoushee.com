0359c2c93038e6a9aae556fb836719ec788d72fd
roszelj <justin@elementwebtech.com>
2011-11-12 19:03:27 -0700

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SalonfousheeCom::Application.routes.draw do
+  get "content/home"
+
+  get "content/staff"
+
+  get "content/careers"
+
+  get "content/login"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ SalonfousheeCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "content#home"
 
   # See how all your routes lay out with "rake routes"
 
