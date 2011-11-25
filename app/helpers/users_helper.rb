f2175d881a112f6ca822950b3753033de5499926
roszelj <justin@elementwebtech.com>
2011-11-25 14:57:12 -0700

    Users models with passwords and show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
-module UsersHelper
+module UsersHelper       
+  
+ def gravatar_for(user, options = { :size => 50 })
+   gravatar_image_tag(user.email.downcase, :alt => user.name,
+                                           :class => 'gravarar',
+                                           :gravatar => options)
+ end
 end
